dbedaba0b65da6cdbcf11c75d66bcdaab4bcc90a
Takuto Yanagida <tokutadaginaya@gmail.com>
2025-10-20 22:39:22 +0900

    Bump versions

--- src/tracker/resource.h
+++ src/tracker/resource.h
@@ -7,10 +7,10 @@
 #define IDC_TRACKER                     109
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
